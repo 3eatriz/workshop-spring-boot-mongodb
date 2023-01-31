d92f06fa1fbf68b31400f67791115de887d9a6e3
Beatriz <beatrizfreitasti@gmail.com>
2023-01-31 01:46:09 -0300

    Inserção de usuário com POST

--- src/main/java/com/beatriz/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/beatriz/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.beatriz.workshopmongo.resources.exception;
 
-import org.apache.logging.log4j.spi.ObjectThreadContextMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
