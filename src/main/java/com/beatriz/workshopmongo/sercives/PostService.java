c31c61fb2a263ccadfbb478d370175ebd36ad625
Beatriz <beatrizfreitasti@gmail.com>
2023-02-05 15:51:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/beatriz/workshopmongo/sercives/PostService.java
@@ -0,0 +1,22 @@
+package com.beatriz.workshopmongo.sercives;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.beatriz.workshopmongo.domain.Post;
+import com.beatriz.workshopmongo.repositoriy.PostRepository;
+import com.beatriz.workshopmongo.sercives.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id){
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
